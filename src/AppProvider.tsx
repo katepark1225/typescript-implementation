6edeac34b9417b02c403b8c64c27a0ca9e7a05da
katepark1225 <117127078+katepark1225@users.noreply.github.com>
2024-01-09 12:07:15 +0900

    Update AppProvider.tsx

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,10 +1,28 @@
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import {ThemeProvider, useTheme} from './theme'
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper'
 
 const AppProvider = ({children}: any) => {
+  const Customtheme = useTheme()
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      ...Customtheme.colors,
+    },
+  }
+
   return (
     <Provider store={store}>
-        {children}
+      <ThemeProvider>
+        <PaperProvider theme={theme}>
+          {children}
+        </PaperProvider>
+      </ThemeProvider>
     </Provider>
   )
 }
