ea0d4db14766ab04d6b5ccb9bf09b5f5f9ce2c36
katepark1225 <117127078+katepark1225@users.noreply.github.com>
2024-01-09 12:06:19 +0900

    Create theme.tsx

---
+++ src/interface/theme.tsx
@@ -0,0 +1,14 @@
+export interface colorProps {
+    primary: string;
+    secondary: string;
+    backgroundColor: string;
+    default: string;
+    input: string;
+    helpText: string;
+    lightContainer: string;
+  }
+  
+  export interface themeProps {
+    colors: colorProps
+  }
+  
